7269d55827a99c7b6d9d375eeeaa9579efb868c6
Marcus F <marcusf.dev@gmail.com>
2024-04-12 17:16:52 +0000

    Added: Set up Login or Create system.

--- run.py
+++ run.py
@@ -14,5 +14,57 @@ SHEET = GSPREAD_CLIENT.open('Eternity Holdings')
 
 accountlist = SHEET.worksheet('accountlist')
 
-data = accountlist.get_all_values()
-print(data)
+def create_account():
+    """
+    Begins the Account creation process.
+    """
+    print("Sending to Account Creation...")
+
+def login_account():
+    """
+    Begins the Account login process.
+    """
+    print("Sending to Account Login...")
+
+def login_or_create():
+    """
+    Gets User string input.
+    Run a while loop to collect a valid string from user
+    via the terminal, which must be the correct value of
+    'Create' or 'Login'. The loop will repeat until input in valid.
+    """
+    print("To proceed with your banking experience, please choose 'Create' or 'Login'")
+    print("Insert the values exactly as shown above.")
+    print("Do not input inverted commas ('')\n")
+
+    while True:
+        mode_str = input("Enter here:\n")
+
+        if validate_mode(mode_str):
+            if mode_str == "Create":
+                print(f"You chose to {mode_str} an Account!")
+                create_account()
+            elif mode_str == "Login":
+                print(f"You chose to {mode_str} to an Account!")
+                login_account()
+
+            break # Ends While loop if Validate returns True
+
+def validate_mode(mode_str):
+    """
+    Checks if the user input in mode_str is Valid.
+    """
+    if mode_str == "Create" or mode_str == "Login":
+        return True
+    else:
+        print(f"Wrong User input of '{mode_str}' detected, this is incorrect. Please try again.")
+        return False
+
+def main():
+    """
+    Run all program functions.
+    """
+    login_or_create()
+
+print("Welcome to Eternity Holdings the #1 App to automate your banking needs!\n")
+main()
